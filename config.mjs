8c75cb065af92c91482482c85b7444b064bb2e66
CyberNinja7-jpg <albertnyasimi27@gmail.com>
2025-08-13 10:42:18 +0300

    Create config.mjs

---
+++ config.mjs
@@ -0,0 +1,7 @@
+export const BOT_NAME = "RAHL-QUANTUM";       // Bot name
+export const PREFIX = ".";                   // Command prefix
+export const OWNER_NUMBER = "254112399557";  // Without @s.whatsapp.net
+export const OWNER_NAME = "Lord Rahl";       // Owner display name
+export const USE_PAIRING_CODE = true;         // true = also show 8-digit pairing code
+export const AUTO_READ = true;                // auto mark messages as read
+export const AUTO_RECONNECT = true;           // reconnect if connection closes
